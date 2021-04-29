28124f4547bb79578f8620114eee49c18fcf0077
gjfzml123 <83047606+gjfzml123@users.noreply.github.com>
2021-04-29 15:19:27 +0900

    bmw0429

---
+++ js/common.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    
+       //로더
+    $(window).load(function(){
+        $('.loader').delay('1000').fadeOut();
+    })
+     
+    
+       //모바일 메뉴
+    $('.mgnb_wrap').hide();
+
+    $('.m_menu').click(function () {
+        $('.mgnb_wrap').fadeIn();
+    });
+
+    $('.close').click(function () {
+        $('.mgnb_wrap').fadeOut();
+    });
+
+
+    
+       //슬릭
+    $('.slider').slick({
+      infinite : true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                      infinite : true,
+                      slidesToShow: 2,
+                      slidesToScroll: 1,
+                      autoplay: true,
+                      autoplaySpeed: 1500,
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                      infinite : true,
+                      slidesToShow: 2,
+                      slidesToScroll: 1,
+                      autoplay: true,
+                      autoplaySpeed: 1500,
+
+                }
+    }
+  ]
+    });
+    
+        //스크롤트리거
+    $(window).scroll(function () {
+        $('.ani_stop').each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() * .3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).removeClass('ani_stop');
+            }
+            if (bottom_of_window < bottom_of_object) {
+                $(this).addClass('ani_stop');
+            }
+        });
+    });
+    
+    
+         // 더보기
+    $(".more_l").slice(0, 0).show(); //showing 3 div
+
+	$(".loadMore").on("click",function(){
+		$(".more_l:hidden").slice(0, 2).show(); //showing 3 hidden div on click
+
+		if($(".more_l:hidden").length ==0)
+		{
+			$(".loadMore").fadeOut(); //this will hide
+			//button when length is 0
+		}
+	})
+
+});
